6f7385fa18358035d315c1db07e929f28de9d2f6
Alex Nelson <51138808+alexjnelson@users.noreply.github.com>
2024-11-25 00:35:42 -0500

    Add middleware to main app (#8)
    * added CORS to main app
    
    * added comment to settings

--- app/services/users.py
+++ app/services/users.py
@@ -17,6 +17,7 @@ def get_user(db: Session, user_id: str) -> Users:
     user = db.exec(statement).first()
     return user
 
+
 def get_user_by_email(db: Session, email: str) -> Users:
     statement = select(Users).where(Users.email == email)
     user = db.exec(statement).first()
