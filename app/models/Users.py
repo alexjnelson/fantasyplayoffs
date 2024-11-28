b935f12d0d31f7dd11a30007340f786a7f21a0a1
imranrhem <imran.rhemtulla@gmail.com>
2024-11-28 17:48:23 -0500

    set up alembic and initialize first migration

--- app/models/Users.py
+++ app/models/Users.py
@@ -1,6 +1,12 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, UniqueConstraint
+from typing import Optional
 
 class Users(SQLModel, table=True):
-    id: str = Field(primary_key=True)
-    name: str = Field()
-    email: str = Field(index=True, unique=True)
+    __tablename__ = "users"
+    __table_args__ = (
+        UniqueConstraint("email", name="users_email_key"),
+    )
+
+    id: Optional[str] = Field(default=None, primary_key=True)
+    name: str
+    email: str
