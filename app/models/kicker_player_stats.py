2e4ed04a68786b69ca0a2bd9dcb908b94556ece5
imranrhem <imran.rhemtulla@gmail.com>
2024-11-28 19:16:04 -0500

    delete residuals

--- app/models/kicker_player_stats.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class KickerPlayerStats(SQLModel, table=True):
-    __tablename__ = "kicker_player_stats"
-    game_id: int = Field(foreign_key="nfl_player_game.id", primary_key=True)
-    fg: float
-    fg_40: float
-    fg_50: float
-    xp: float
